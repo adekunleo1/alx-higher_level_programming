82d867a3f3ea6e5d09a28956a82c551cac3a4dca
adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-09-27 04:12:46 -0700

    Object Relational Mapping with Python

--- 0x0F-python-object_relational_mapping/1-filter_state.py
+++ 0x0F-python-object_relational_mapping/1-filter_state.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+    Lists all states with a name starting with N(upper N) from the database hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
