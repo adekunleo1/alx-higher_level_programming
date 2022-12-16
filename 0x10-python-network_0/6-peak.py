73369dc4a480dc2dee205e2d135b8500dc046087
adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-12-16 03:43:47 +0100

    peak py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Finding a peak in a list of unsorted integers """
+""" Test function find peak """
 
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
