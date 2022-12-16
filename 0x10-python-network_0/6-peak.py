ebca35654f376be32da533d190cb2403385a739b
adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-12-16 03:40:38 +0100

    Peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Finds a peak using algorithm in an unsorted integer """
+""" Finding a peak in a list of unsorted integers """
 
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
