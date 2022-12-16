9ee32eab8b31ecf0afa20c00eac26bce02feefe9
adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-12-16 03:17:22 +0100

    catch update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to 0.0.0.0:5000/catch_me that that causes the server to respond with a message "You got me!"
+# Script that makes a request to 0.0.0.0:5000/catch_me causing the server to respond with a message "You got me!"
 curl -sL http://0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: You got me!"
