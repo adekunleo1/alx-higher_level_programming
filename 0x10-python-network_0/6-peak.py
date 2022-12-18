9112b6960fcf93ede448665ee22306bfeaf36f1b
adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-12-18 13:11:01 +0100

    Python Network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-""" Test function find peak """
+"""finds the peak of an unsorted list"""
+
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
+    """finds the peak of a list"""
 
-    if list_of_integers is None or list_of_integers == []:
+    if list_of_integers == []:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    li = list_of_integers
+    length = len(li)
+    mid = int(length/2)
+
+    if mid-1 < 0 and mid+1 >= length:
+        return li[mid]
+    elif mid-1 < 0:
+        if li[mid] > li[mid+1]:
+            return li[mid]
+        else:
+            return li[mid+1]
+    elif mid+1 >= length:
+        if li[mid] > li[mid-1]:
+            return li[mid]
+        else:
+            return li[mid-1]
+
+    if li[mid] > li[mid+1] and li[mid] > li[mid-1]:
+        return li[mid]
+    else:
+        pass
+
+    if li[mid + 1] > li[mid - 1]:
+        return find_peak(li[mid:])
+    return find_peak(li[:mid])
